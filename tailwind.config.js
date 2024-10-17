5fef25d92c91b47b9f0b7db2dda09e3d638b7f28
HomayoonAAhmad <homayoon5600@gmail.com>
2024-10-17 14:19:51 +0330

    header sectio(logo not complete)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./*.html"], // Scan HTML files for classes
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        vazirmatn: ["Vazirmatn", "sans-serif"],
+      },
+      fontSize: {
+        "custom-lg": "16px",
+        "custom-sm": "10px",
+      },
+      borderRadius: {
+        20: "20px", // Custom border-radius of 20px
+      },
+    },
   },
   variants: {
     extend: {},
