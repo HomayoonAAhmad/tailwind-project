d1f4ab36185578f808f3cb8212a53b12b66a5a50
HomayoonAAhmad <homayoon5600@gmail.com>
2024-10-21 09:50:27 +0330

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     extend: {
       fontFamily: {
         vazirmatn: ["Vazirmatn", "sans-serif"],
+        nastaliq: ["IranNastaliq", "sans-serif"], // Fallback to sans-serif
+        yekan: ["Yekan", "sans-serif"], // Fallback to sans-serif
       },
       fontSize: {
         "custom-lg": "16px",
@@ -13,10 +15,13 @@ module.exports = {
         20: "20px", // Custom border-radius of 20px
         10: "10px",
       },
+      textShadow: {
+        nastaliq: "0 4px 4px rgba(0, 0, 0, 0.25)",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwindcss-textshadow")],
 };
