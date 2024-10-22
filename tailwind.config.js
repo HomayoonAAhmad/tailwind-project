4a4104782d9847904019f034949c110d8042d2a8
HomayoonAAhmad <homayoon5600@gmail.com>
2024-10-22 21:52:51 +0330

    idk

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,12 +24,6 @@ module.exports = {
         "gradient-light": "#485A75",
         "gradient-dark": "#293A51",
       },
-      width: {
-        "social-icon-width": "42px",
-      },
-      height: {
-        "social-icon-height": "42px",
-      },
     },
   },
   variants: {
